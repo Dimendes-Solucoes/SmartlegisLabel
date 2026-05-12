78e269e603dff9a9fdddcfbef6698d0d965f43f2
felipemarley <felipemarley34@gmail.com>
2026-05-12 16:45:01 -0300

    fix: function_label in member card

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -26,6 +26,10 @@
         <span class="px-3 py-1 text-xs font-medium rounded-full" style="background-color: rgba(0, 122, 184, 0.15); color: #007AB8;">
           {{ member.category_party?.name_party || 'Partido não informado' }}
         </span>
+        <span 
+          v-if="member.function_label" class="px-3 py-1 text-xs font-medium rounded-full" style="background-color: rgba(0, 122, 184, 0.15); color: #007AB8;">
+          {{ member.function_label }}
+        </span>
       </div>
     </div>
 
